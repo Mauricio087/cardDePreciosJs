56b38b05cb4ea45bfbcf9d090362204ff37f97ba
mauricio087 <m.river.cea@gmail.com>
2024-11-23 22:35:38 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const precio = 400000;
+let cantidad = 0;
+
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadSpan = document.querySelector(".cantidad");
+const valorTotalSpan = document.querySelector(".valor-total");
+const btnMas = document.querySelector(".btn-mas");
+const btnMenos = document.querySelector(".btn-menos");
+
+precioSpan.innerHTML = precio;
+
+btnMas.addEventListener("click",()=>{
+    cantidad = cantidad + 1;
+    cantidadSpan.innerHTML = cantidad;
+    valorTotalSpan.innerHTML = precio * cantidad;
+});
+
+btnMenos.addEventListener("click",()=>{
+    cantidad = cantidad - 1;
+    cantidadSpan.innerHTML = cantidad;
+    valorTotalSpan.innerHTML = precio * cantidad;
+});
